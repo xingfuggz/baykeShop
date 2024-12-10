fdf3b0781b5cba6964f685a29df8cfebf0ddb9f6
轻编程 <2539909370@qq.com>
2024-12-10 09:38:39 +0800

    fix - 修复支付宝支付完成之后回调404问题，去除异步通知的csrf验证

--- src/baykeshop/contrib/shop/urls.py
+++ src/baykeshop/contrib/shop/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    # 首页
-    path('', views.BaykeShopIndexView.as_view(), name='index'),
+    # 支付宝支付回调(必须在第一个)
+    path('pay/callback/', views.AlipayCallbackView.as_view(), name='alipay-callback'),   
     # 商品列表
     path('list/', views.BaykeShopGoodsListView.as_view(), name='list'),
     # 搜索
@@ -23,6 +23,6 @@ urlpatterns = [
     re_path(r'^carts/(?P<skuids>\d+(?:,\d+)*)/$', views.BaykeShopCashView.as_view(), name='cash_cartids'),
     # 支付页面
     path('pay/<slug:order_sn>/', views.BaykeShopOrdersPayView.as_view(), name='orders-pay'),
-    # 支付宝支付回调
-    path('pay/callback/', views.AlipayCallbackView.as_view(), name='alipay-callback'),
+    # 首页
+    path('', views.BaykeShopIndexView.as_view(), name='index'),
 ]
--- src/baykeshop/urls.py
+++ src/baykeshop/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('baykeshop.contrib.shop.urls')),
     path('member/', include('baykeshop.contrib.member.urls')),
     path('article/', include('baykeshop.contrib.article.urls')),
+    path('', include('baykeshop.contrib.shop.urls')),
     # 接口url
     path('api/', include('baykeshop.api.urls', namespace='baykeshop_api')),
 ]
