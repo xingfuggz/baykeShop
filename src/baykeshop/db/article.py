e31dbcee2240e2370c652bc376cdcdb259f8bb5c
轻编程 <2539909370@qq.com>
2024-12-24 21:07:22 +0800

    style - 整理代码格式

--- src/baykeshop/db/article.py
+++ src/baykeshop/db/article.py
@@ -9,14 +9,18 @@ User = get_user_model()
 
 
 class BaseArticleModel(BaseModel):
-    """ 文章基类模型 """
-    title = models.CharField(max_length=50, verbose_name=_('标题'))
-    # content = models.TextField(verbose_name=_('内容'))
-    content = RichTextField(verbose_name=_('内容'))
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('作者'), blank=True, null=True)
-    image = models.ImageField(upload_to='article', blank=True, null=True, verbose_name=_('图片'))
-    order = models.IntegerField(default=0, verbose_name=_('排序'))
+    """文章基类模型"""
+
+    title = models.CharField(max_length=50, verbose_name=_("标题"))
+    content = RichTextField(verbose_name=_("内容"))
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name=_("作者"), blank=True, null=True
+    )
+    image = models.ImageField(
+        upload_to="article", blank=True, null=True, verbose_name=_("图片")
+    )
+    order = models.IntegerField(default=0, verbose_name=_("排序"))
 
     class Meta:
-        ordering = ['-order', '-created_time']
+        ordering = ["-order", "-created_time"]
         abstract = True
